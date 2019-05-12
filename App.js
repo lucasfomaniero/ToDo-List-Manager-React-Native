e537043fc56df2162ac3c2bd7bc1fb43fe57bff2
lucasfomaniero <lucasfomaniero@gmail.com>
2019-05-12 13:26:26 -0300

    Created the Register screen

--- App.js
+++ App.js
@@ -8,7 +8,8 @@
 
 import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
-import Login from './screens/Login';
+// import Login from './screens/Login';
+import Register from './screens/Register';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,7 +23,7 @@ export default class App extends Component<Props> {
   render() {
 
     return (
-      <Login/>
+      <Register/>
     );
   }
 }
