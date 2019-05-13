373cbb9c47e3ce8ed5fba595991f9cc3a33c69ed
lucasfomaniero <lucasfomaniero@gmail.com>
2019-05-13 20:46:41 -0300

    Created the navigation between screens

--- App.js
+++ App.js
@@ -10,20 +10,16 @@ import React, { Component } from 'react';
 import { Platform, StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
 // import Login from './screens/Login';
 import Register from './screens/Register';
+import Routes from './src/Routes/Routes';
+import Login from './screens/Login';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+
 
 
 export default class App extends Component<Props> {
   render() {
-
     return (
-      <Register/>
+      <Routes/>
     );
   }
 }
